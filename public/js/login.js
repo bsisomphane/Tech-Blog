55a951427989d2cb311959b5d5753759bf9efd57
Bailey Sisomphane <bsisomphane7@gmail.com>
2022-01-04 19:30:27 -0600

    public js

--- public/js/login.js
+++ public/js/login.js
@@ -15,7 +15,7 @@ const loginFormHandler = async (event) => {
 
     if (response.ok) {
       // If successful, redirect the browser to the profile page
-      document.location.replace('/profile');
+      document.location.replace('/');
     } else {
       alert(response.statusText);
     }
@@ -25,25 +25,40 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#name-signup').value.trim();
+  const name = document.querySelector('#username-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
   if (name && email && password) {
     const response = await fetch('/api/users', {
       method: 'POST',
-      body: JSON.stringify({ name, email, password }),
+      body: JSON.stringify({ username, email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      console.log(response);
+    } else {
+      alert(response.statusText);
+    }
+
+    const responseTwo = await fetch("/api/users/login", {
+      method: "post",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (responseTwo.ok) {
+      console.log(response, " Logged in successfully!");
+      document.location.replace("/");
     } else {
       alert(response.statusText);
     }
   }
-};
-
+}
 document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
---
+++ public/js/update-post.js
@@ -0,0 +1,29 @@
+const createPostHandler = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector("#post-title").value.trim();
+    const body = document.querySelector("#post-body").value.trim();
+    const post_id = window.location.toString().split("/")[
+      window.location.toString().split("/").length - 1
+    ];
+
+    if (body) {
+      const response = await fetch(`/api/posts/` = post_id, {
+        method: 'PUT',
+        body: JSON.stringify({title, body}),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace("/dashboard");
+      } else {
+        alert('Failed to update post');
+      }
+    }
+  };
+  
+  document
+  .querySelector("#create-post-btn")
+  .addEventListener("click", createPostHandler);
